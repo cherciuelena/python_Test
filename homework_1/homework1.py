fe1357b7c71665a638ea7d6140b6a71d95a3e42a
Elena <uRSA0420AprILIE@Beja>
2022-07-12 23:02:47 +0300

    homework1_data_structures

--- homework_1/homework1.py
+++ homework_1/homework1.py
@@ -1,13 +1,13 @@
 # 1.Declarare lista
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-my_list_asc = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-my_list_desc = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
 # 2.Ordonare ascendenta lista
+my_list_asc = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 my_list_asc.sort()
 print(my_list_asc)
 
 # 3.Ordonare descendenta lista
+my_list_desc = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 my_list_desc.sort(reverse=True)
 print(my_list_desc)
 
