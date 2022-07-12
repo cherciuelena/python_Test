be4b68cba9d61c849a25280fbd4517a69ba083ac
Elena <uRSA0420AprILIE@Beja>
2022-07-12 18:42:46 +0300

    data_structures

---
+++ curs_3_structuri_de_date/data_structures.py
@@ -0,0 +1,11 @@
+# LISTE []
+    # ordonate - pot fi indexate si mutabile - poate fi modificata
+    
+# TUPLURI ()
+    # ordonate si immutabile
+    
+# DICTIONARE {} key:value
+    # neordonat si mutabil
+
+# SETURI {}
+    # neordonat si immutabil
