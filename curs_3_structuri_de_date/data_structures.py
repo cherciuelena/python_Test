c309fa655cb081c2e7c0da96e57df210c1fedc88
Elena <uRSA0420AprILIE@Beja>
2022-07-13 13:48:29 +0300

    data structures

--- curs_3_structuri_de_date/data_structures.py
+++ curs_3_structuri_de_date/data_structures.py
@@ -1,11 +1,25 @@
 # LISTE []
     # ordonate - pot fi indexate si mutabile - poate fi modificata
+    # permite elemente duplicat
+    # -1 - ultimul elem
+    # slicing
+    # concatenare
     
 # TUPLURI ()
-    # ordonate si immutabile
+    # ordonate si imutabile
+    # permite elemente duplicat
+    # slicing
+    # concatenare
+    # NU permite op de adaugare stergere pt ca e structura imutabila
     
 # DICTIONARE {} key:value
-    # neordonat si mutabil
+    # ORDONATE PYTHON 3.7 (au fost neordonate pana la python 3.6) si mutabil
+    # chiele sunt unice , valotile pot fi duplicat
+    # cheia poate fi nr, string sau chiar tuplu
 
 # SETURI {}
     # neordonat si immutabil
+    # elementele din set sunt unice
+    # accesarea unui element din set nu e posibila dar setul poate fi parsat(parcurs + filtrat/analizat-faci ceva)
+    # elementele din set nu isi pastreaza ordinea
+    
